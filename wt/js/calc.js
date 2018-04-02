d0ae3ab49515e73e79a34215e580ca53b002c2ec
nekita6 <33751985+nekita6@users.noreply.github.com>
2018-04-02 19:47:52 +0300

    calc!

--- wt/js/calc.js
+++ wt/js/calc.js
@@ -6,22 +6,25 @@ function calc() {
 	weight = document.getElementById('weighth').value;
 	wrap = document.getElementById('wraph').value;
 	insurance = document.getElementById('insuranceh').value;
-	
+
 	if (long =='' || weight =='' || wrap =='' || insurance =='')
-		alert('Немножко пусто!');
-	
-	long = parseInt(long); 
-	weight = parseInt(weight);
-	wrap = parseInt(wrap);
-	insurance = parseInt(insurance);
+		alert('Немножко пусто!')
+	else {
+		long = parseInt(long); 
+		weight = parseInt(weight);
+		wrap = parseInt(wrap);
+		insurance = parseInt(insurance);
+	}
 
-	result = 
-
-		(long > 2000) ? 'ОШИБКА!<br>Максимальный путь перевозки <b>2000км</b>' :
-		(weight > 40) ? 'ОШИБКА!<br>Максимальный вес груза <b>40кг</b>' :
-		(long < 0 || weight < 0 || wrap < 0 || insurance < 0) ? 'Вы ввели отрицательное значение!<br><b>НЕ НАДО ТАК!</b>' :
-
-	long*21 + weight*10 + wrap + insurance  ;
+	if (long > 2000)
+		result = 'ОШИБКА!<br>Максимальный путь перевозки <b>2000км</b>'
+	else if (weight > 40)
+		result = 'ОШИБКА!<br>Максимальный вес груза <b>40кг</b>'
+	else if (long < 0 || weight < 0 || wrap < 0 || insurance < 0)
+		result = 'Вы ввели отрицательное значение!<br><b>НЕ НАДО ТАК!</b>'
+	else {
+		result = long*21 + weight*10 + wrap + insurance;
+	}
 
 	document.getElementById('out').innerHTML = result;
 
