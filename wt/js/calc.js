05a3ed9b22a94dd4ccf5e566d8b5c2d017b5cb23
nekita6 <33751985+nekita6@users.noreply.github.com>
2018-04-06 14:49:41 +0300

    js fix

--- wt/js/calc.js
+++ wt/js/calc.js
@@ -8,14 +8,20 @@ function calc() {
 
 	if (long =='' || weight =='' || wrap =='' || insurance =='')
 		alert('Немножко пусто!')
-	else if (valid.test(long, weight, wrap, insurance) == true || valid.test(weight) == true || valid.test(wrap) == true || valid.test(insurance) == true)
-		alert('Вводите только числовые значение, пожалуйста')
-	else if (long > 2000)
-		result = 'ОШИБКА!<br>Максимальный путь перевозки <b>2000км</b>'
-	else if (weight > 40)
-		result = 'ОШИБКА!<br>Максимальный вес груза <b>40кг</b>'
-	else if (long < 0 || weight < 0 || wrap < 0 || insurance < 0)
-		result = 'Вы ввели отрицательное значение!<br><b>НЕ НАДО ТАК!</b>'
+	
+	else if (valid.test(long) == true || valid.test(weight) == true || valid.test(wrap) == true || valid.test(insurance) == true)
+		alert('Вводите только положительные числовые значение, пожалуйста')
+	
+	else if (long > 2000){
+		result = 'ОШИБКА!<br>Максимальный путь перевозки <b>2000км</b>';
+		alert(result)
+	}
+	
+	else if (weight > 40){
+		result = 'ОШИБКА!<br>Максимальный вес груза <b>40кг</b>';
+		alert(result)
+	}
+	
 	else {
 		result = long*21 + weight*10 + wrap*1 + insurance*1;
 		document.getElementById('out').innerHTML = result + ' ₽';
