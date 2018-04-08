0c1725c0e5cd7bbf1dc7db580bade1b514e4dd43
nekita6 <33751985+nekita6@users.noreply.github.com>
2018-04-09 01:40:38 +0300

    fsafafaf

--- wt/js/calc.js
+++ wt/js/calc.js
@@ -42,12 +42,19 @@ function calc() {
 	var weight = document.getElementById('weighth').value;
 	var wrap = document.getElementById('wraph').value;
 	var insurance = document.getElementById('insuranceh').value;
+	var valid = /[^\d,.]/g;
 
 	if (long =='' || weight =='')
 		alert('Немножко пусто! \nДлина пути и вес товара обязательны для заполнения')
 
 	else if (long == 0 || weight == 0)
 		alert('Длина пути и вес не могут быть равны 0')
+
+	else if (long =='.' || weight =='.' || wrap =='.' || insurance =='.' || long ==',' || weight ==',' || wrap ==',' || insurance ==',')
+		alert('Вводите только положительные числовые значение.')
+
+	else if (valid.test(long) == true || valid.test(weight) == true || valid.test(wrap) == true || valid.test(insurance) == true)
+		alert('Вводите только положительные числовые значение.')
 	
 	else if (long > 2000){
 		result = 'ОШИБКА! Максимальный путь перевозки 2000км';
