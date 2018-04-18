f8f74a0c5eb9f972404fef88beae171c9f627f94
nekita6 <33751985+nekita6@users.noreply.github.com>
2018-04-18 19:23:53 +0300

    фывфвфвфв

--- wt/js/menu.js
+++ wt/js/menu.js
@@ -1,9 +1,6 @@
 var menu = document.querySelector('.menu')
 var menu_icon = document.querySelector('.menu_icon')
 var open_icon = document.querySelector('.open_icon')
-var change;
-var touch;
-var	startingX;
 
 function openMenu() {
 
@@ -23,31 +20,6 @@ function closeMenu() {
 
 menu_icon.onclick = function() { openMenu() }
 open_icon.onclick = function() { closeMenu() }
-
-document.ontouchstart = function startTouches(e) {
-	
-	startingX = e.touches[0].clientX;
-
-}
-
-document.ontouchmove = function moveTouches(e) {
-	
-	touch = e.touches[0];
-	change = startingX - touch.clientX;
-
-	if (change < 0) { menu.style.left = 'calc(-100% - ' + change + 'px)' }
-
-	else { menu.style.left = 'calc(-100% + ' + change + 'px)' }
-
-}
-
-document.ontouchend = function moveTouches(e) {
-	
-	if (change < -150) { openMenu() }
-
-	else { closeMenu() }
-	
-}
 
 window.onresize = function(event) {
 
